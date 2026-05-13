2d806c2a55d4d5de138dbae766ece7e792049aa2
58157667 <58157667@qq.com>
2026-05-13 14:37:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-const API_URL =
-    "https://yoyaku8.onrender.com/api/reservations";
-
-const FORMSPREE_URL =
-    "https://formspree.io/f/mqenwlpa";
-
 const form =
     document.getElementById("bookingForm");
 
-const message =
-    document.getElementById("message");
-
-form.addEventListener("submit", async (e) => {
+form.addEventListener("submit", (e) => {
 
     e.preventDefault();
-
-    message.innerHTML = "";
 
     const booking = {
 
@@ -38,74 +27,14 @@ form.addEventListener("submit", async (e) => {
 
     };
 
-    try {
+    // 保存到浏览器
+    localStorage.setItem(
+        "booking",
+        JSON.stringify(booking)
+    );
 
-        // 1. 发送到 Spring Boot
-
-        const response = await fetch(API_URL, {
-
-            method: "POST",
-
-            headers: {
-                "Content-Type": "application/json"
-            },
-
-            body: JSON.stringify(booking)
-
-        });
-
-        const result = await response.text();
-
-        // 时间冲突
-
-        if (!response.ok) {
-
-            message.innerHTML =
-                "<p style='color:red'>" + result + "</p>";
-
-            return;
-        }
-
-        // 2. 发送 Formspree 邮件
-
-        const mailResponse =
-            await fetch(FORMSPREE_URL, {
-
-                method: "POST",
-
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                },
-
-                body: JSON.stringify(booking)
-
-            });
-
-        // 邮件失败
-
-        if (!mailResponse.ok) {
-
-            message.innerHTML =
-                "<p style='color:red'>メール送信失敗</p>";
-
-            return;
-        }
-
-        // 成功
-
-        message.innerHTML =
-            "<p style='color:green'>予約成功</p>";
-
-        form.reset();
-
-    } catch (error) {
-
-        console.error(error);
-
-        message.innerHTML =
-            "<p style='color:red'>システムエラー</p>";
-
-    }
+    // 跳转确认页面
+    window.location.href =
+        "confirm.html";
 
 });
